f67251e1fefa55100cf69d70bd7307c38d2368ff
japa1504 <kekenozaki4@gmail.com>
2026-05-18 22:20:50 -0300

    Correção

--- 2-BIM-AtividadeM1/FourMacia-editado/FourMacia/excluir.php
+++ 2-BIM-AtividadeM1/FourMacia-editado/FourMacia/excluir.php
@@ -2,14 +2,15 @@
 
 require_once 'config/conexao.php';
 
-?>
+$id = isset($_GET['id']) ? (int) $_GET['id'] : 0;
 
-<?php
+if ($id > 0) {
 
-$id = isset($_GET['id'])? (int)$_GET['id'] : 0;
-if($id > 0){
-	$del = $pdo->prepare('DELETE FROM produtos WHERE id = :id');
-	$del->execute([':id'=>$id]);
+    $del = $conexao->prepare('DELETE FROM produtos WHERE id = :id');
+
+    $del->execute([
+        ':id' => $id
+    ]);
 }
 
 header('Location: index.php');
